bcd2926385fa0c63c5cfe350c86530381a6282ce
a272714 <a272714@naver.com>
2018-11-30 13:46:53 +0900

    <Manifest.xml> 추후 Activity 수정 및 추가 시 한 눈에 보기 위해 Linear 형태로 정렬함 <BoardActivity.java> Board Activity 추가. Calender 구현 필요 <activity_board.xml> 위의 xml 추가. 현재 내부 구현 예정

---
+++ app/src/main/java/e/a2727/project/BoardActivity.java
@@ -0,0 +1,56 @@
+package e.a2727.project;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.LinearLayout;
+
+public class BoardActivity extends AppCompatActivity {
+
+    private LinearLayout linearlayout1, linearlayout2, linearlayout3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_board);
+        linearlayout1 = findViewById(R.id.linearLayout1);
+        linearlayout2 = findViewById(R.id.linearLayout2);
+        linearlayout3 = findViewById(R.id.linearLayout3);
+
+        linearlayout1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Calender class 구현 시 풀어주세요
+//                startActivity(new Intent(BoardActivity.this, CalenderActivity.class));
+//                finish();
+            }
+        });
+
+        linearlayout2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //자유 게시판 class 구현 시 풀어주세요
+//                startActivity(new Intent(BoardActivity.this, FreeActivity.class));
+//                finish();
+            }
+        });
+
+        linearlayout3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TIPS 구현 시 풀어주세요
+//                startActivity(new Intent(BoardActivity.this, TipsActivity.class));
+//                finish();
+            }
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        startActivity(new Intent(BoardActivity.this, MainActivity.class));
+        finish();
+    }
+
+}
