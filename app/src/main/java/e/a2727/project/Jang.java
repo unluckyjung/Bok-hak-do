db0313b74dff5c63a96e3ced1cef0eb95b187ab7
UnluckyJung <ybook2012@gmail.com>
2018-12-15 18:56:11 +0900

    쓰잘때기 없는것(이미지, 주석삭제함)

--- app/src/main/java/e/a2727/project/Jang.java
+++ app/src/main/java/e/a2727/project/Jang.java
@@ -1,6 +1,7 @@
 package e.a2727.project;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -8,22 +9,30 @@ import android.widget.Button;
 
 public class Jang extends AppCompatActivity {
 
-    private Button button3;
+    private Button button1, button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jang);
-        button3 = findViewById(R.id.Backed);
+        button1 = findViewById(R.id.JangHome);
+        button2 = findViewById(R.id.ITHome);
 
-        button3.setOnClickListener(new View.OnClickListener() {
+        button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(Jang.this, ProfessorActivity.class));
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://dblab.jbnu.ac.kr/new")));
                 finish();
             }
         });
 
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://it.jbnu.ac.kr/itjbnu/2016/inner.php?sMenu=main")));
+                finish();
+            }
+        });
     }
 
 
